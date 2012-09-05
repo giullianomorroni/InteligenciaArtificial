579427294001f987c8399e247f6215f7c280a04b
Giulliano Morroni <giullianomorroi@gmail.com>
2012-09-04 23:27:06 -0300

    atualizacao

--- src/capitulo_6/arvore_decisao.py
+++ src/capitulo_6/arvore_decisao.py
@@ -8,6 +8,8 @@ from galho import Galho
 
 class ArvoreDecisao(object):
 
+    nvl = 1;
+
     def dados(self):
         d = [
         {'nome':'Angelica',       'idade':23,    'sexo':'f',    'estado':'casado',     'renda':'alta',      'escolaridade':'superior'},
@@ -15,35 +17,90 @@ class ArvoreDecisao(object):
         {'nome':'Giulliano',      'idade':32,    'sexo':'m',    'estado':'casado',     'renda':'media',     'escolaridade':'colegial'},
         {'nome':'Jose',           'idade':22,    'sexo':'m',    'estado':'casado',     'renda':'media',     'escolaridade':'colegial'},
         {'nome':'Carol',          'idade':45,    'sexo':'f',    'estado':'solteiro',   'renda':'baixa',     'escolaridade':'superior'},
+        {'nome':'Gabriela',       'idade':65,    'sexo':'f',    'estado':'solteiro',   'renda':'media',     'escolaridade':'colegial'},
         {'nome':'Carlos',         'idade':65,    'sexo':'m',    'estado':'viuvo',      'renda':'baixa',     'escolaridade':'superior'}]
         return d;
-    
+
     def divisao(self, coluna, valor, valores=None):
         if valores == None:
             valores = self.dados()
         escolhidos = []
         outros = []
-        #valores = self.dados()
         for v in valores:
             if v[coluna] == valor:
                 escolhidos.append(v)
             else:
                 outros.append(v) 
         d = []
-        d.append(Galho().registrar(coluna, valor, 'Raiz', escolhidos))
-        d.append(Galho().registrar('outros', valor, 'Raiz', outros))
+        g1 = Galho(coluna, valor, self.nvl)
+        g1.registrarValoresInteressantes(escolhidos)
+        g1.registrarValoresNaoInteressantes(outros)
+        d.append(g1)
         return d
 
-    def segunda_divisao(self, coluna, valor, galho, pai):
-        escolhidos = []
-        outros = []
-
-        for v in galho.valores:
-            if v[coluna] == valor:
-                escolhidos.append(v)
+    def nova_divisao(self, coluna, valor, galhos):
+        self.nvl += 1
+        for galho in galhos:
+            valores = []
+            if len(galho.filhos) > 0:
+                valores = galho.filhos
             else:
-                outros.append(v) 
-        d = []
-        d.append(Galho().registrar(coluna, valor, pai, escolhidos))
-        d.append(Galho().registrar('outros', valor, pai, outros))
-        return d
+                valores = galhos
+
+            for filho in valores:
+                escolhidos = []
+                outros = []
+                g1 = Galho(coluna, valor, self.nvl)
+                valores = filho.interessa
+                for v in valores:
+                    if v[coluna] == valor:
+                        escolhidos.append(v)
+                    else:
+                        outros.append(v)
+                g1.registrarValoresInteressantes(escolhidos)
+                g1.registrarValoresNaoInteressantes(outros)
+                galho.adicionarFilho(g1)
+
+        for galho in galhos:
+            valores = []
+            if len(galho.filhos) > 0:
+                valores = galho.filhos
+            else:
+                valores = galhos
+
+            for filho in valores:
+                escolhidos = []
+                outros = []
+                g1 = Galho(coluna, valor, self.nvl)
+                valores = filho.n_interessa
+                for v in valores:
+                    if v[coluna] == valor:
+                        escolhidos.append(v)
+                    else:
+                        outros.append(v)
+                g1.registrarValoresInteressantes(escolhidos)
+                g1.registrarValoresNaoInteressantes(outros)
+                galho.adicionarFilho(g1)                
+        return galhos
+
+    def imprimir(self, galhos):
+        self.imprimirGalho(galhos, (1))
+
+    def imprimirGalho(self, galhos, line):
+        print str(galhos[line-1])
+        for galho in galhos:
+            print '    Interessa:'
+            for v in galho.interessa:
+                print '        ' + str(v)
+            print '    Nao Interessa:'
+            for v in galho.n_interessa:
+                print  '        ' + str(v)
+            if len(galho.filhos) > 0:
+                print str(galho.filhos[line-1])
+            for filho in galho.filhos:
+                print '    Interessa:'
+                for v in filho.interessa:
+                    print '        ' + str(v)
+                print '    Nao Interessa:'
+                for v in filho.n_interessa:
+                    print  '        ' + str(v)
--- src/capitulo_6/galho.py
+++ src/capitulo_6/galho.py
@@ -6,17 +6,44 @@ Created on 30/08/2012
 
 class Galho(object):
 
-    global nome;
-    global divisor;
-    global pai;
-    global valores;
+    nome = None
+    divisor = None
+    filhos = None
+    nivel = None
 
-    def registrar(self, nome, divisor, pai, valores):
+    interessa = None
+    n_interessa = None
+
+    def __init__(self, nome, divisor, nvl):
+        self.nivel = nvl
         self.nome = nome;
-        self.divisor = divisor
-        self.pai = pai;
-        self.valores = valores;
+        self.divisor = divisor        
+        self.filhos = [];
+        self.interessa = []
+        self.n_interessa = []
+
+    def registrarValoresInteressantes(self, valores):
+        self.interessa += valores;
         return self
 
+    def registrarValoresNaoInteressantes(self, valores):
+        self.n_interessa += valores;
+        return self
+
+    def adicionarFilho(self, filho):
+        if (filho in self.filhos):
+            f = self.filhos.pop( self.filhos.index(filho) )
+            f.n_interessa += filho.n_interessa
+            f.interessa  += filho.interessa
+            self.filhos.append(f)
+        else:
+            self.filhos.append(filho)
+
     def __str__(self):
-        return ' -> Galho: ' + self.nome + ' Divisor:' + str(self.divisor) + ' Pai:' + str(self.pai) + ' Valores:' + str(self.valores)
+        return 'Galho: %s (%s) Nv: %s' % (self.nome, str(self.divisor), str(self.nivel)) ;
+
+    def __cmp__(self, other):
+        if self.nome == other.nome:
+            return 0
+        else:
+            return 1
