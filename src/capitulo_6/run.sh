e207ec89ea2472f4c7e676a608f0e7303ec06cae
Giulliano Morroni <giullianomorroi@gmail.com>
2012-09-04 23:27:10 -0300

    atualizacao

--- src/capitulo_6/run.sh
+++ src/capitulo_6/run.sh
@@ -1,20 +1,14 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from arvore_decisao import ArvoreDecisao 
+from arvore_decisao import ArvoreDecisao
+ 
 a = ArvoreDecisao()
-valores = a.divisao('idade', 65);
-print 'Selecionados ' + str(valores[0])
-print 'Outros ' + str(valores[1])
-print '\n'
 
-novos_valores = a.segunda_divisao('sexo', 'm', valores[0], valores[0].nome)
-print '\n'
-print 'Selecionados ' + str(novos_valores[0])
-print 'Outros ' + str(novos_valores[1])
+v1 = a.divisao('sexo', 'm');
 
-novos_valores = a.segunda_divisao('sexo', 'm', valores[1], valores[0].nome)
-print '\n'
-print 'Selecionados ' + str(novos_valores[0])
-print 'Outros ' + str(novos_valores[1])
+#v2 = a.nova_divisao('sexo', 'm', v1)
 
+v2 = a.nova_divisao('renda', 'alta', v1)
+
+a.imprimir(v2)
