3f0f61a98eae747bbb480860bab205f901176b95
Giulliano Morroni <giullianomorroni@gmail.com>
2013-02-21 12:01:56 -0300

    Alteração das estruturas de pastas

--- src/capitulo_4/run.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
-
-from Crawler import Crawler
-c = Crawler('searchindex.db')
-#c.createindextables()
-c.crawl(['http://giullianomorroni.com/portal'])
