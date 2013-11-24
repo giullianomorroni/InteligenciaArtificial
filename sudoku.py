f982f0d67bcdcb29f06a9e42028f34d662668ff5
giulliano <giulliano@debian.giulliano>
2013-11-24 21:57:08 -0200

    sudoku

--- sudoku.py
+++ sudoku.py
@@ -3,21 +3,25 @@
 
 import random
 
-linha1 = [1,2,3,4,5,6,7,8,9]
-linha2 = [1,2,3,4,5,6,7,8,9]
-linha3 = [1,2,3,4,5,6,7,8,9]
-linha4 = [1,2,3,4,5,6,7,8,9]
-linha5 = [1,2,3,4,5,6,7,8,9]
-linha6 = [1,2,3,4,5,6,7,8,9]
-linha7 = [1,2,3,4,5,6,7,8,9]
-linha8 = [1,2,3,4,5,6,7,8,9]
-linha9 = [1,2,3,4,5,6,7,8,9]
-
-tabuleiro = [ linha1, linha2, linha3, linha4, linha5, linha6, linha7, linha8, linha9 ]
-valido = True
+linha1 = [0,2,3,0,5,0,7,8,9]
+linha2 = [1,0,3,0,5,0,7,8,9]
+linha3 = [1,2,0,4,5,0,7,8,0]
+linha4 = [1,2,3,4,5,0,7,0,9]
+linha5 = [1,2,0,4,5,6,7,8,9]
+linha6 = [1,0,3,4,0,6,7,0,9]
+linha7 = [1,2,0,4,5,6,7,8,0]
+linha8 = [1,2,0,4,5,6,0,8,9]
+linha9 = [1,0,3,4,0,6,0,8,9]
 
 def verificarDuplicidadeVertical(linhax, linhay):
 	for i in range(0,9):
+		print linhax
+		print linhay
+		print ' '
+
+		if linhax[i] == 0: return True;
+		elif linhay[i] == 0: return True;
+
 		if linhax[i] == linhay[i]:
 			return False;
 	return True;
@@ -25,6 +29,9 @@ def verificarDuplicidadeVertical(linhax, linhay):
 def verificarDuplicidadeHorizontal(linhax):
 	for x in range(0,9):
 		for y in range(x+1,9):
+			if linhax[x] == 0: return True;
+			elif linhax[y] == 0: return True;
+
 			if linhax[x] == linhax[y]:
 				return False;
 	return True;
@@ -40,20 +47,25 @@ def gerarTabuleiro():
 	random.shuffle(linha7)
 	random.shuffle(linha8)
 	random.shuffle(linha9)
+	tabuleiro = [ linha1, linha2, linha3, linha4, linha5, linha6, linha7, linha8, linha9 ]
 
 	for x in range(0,9):
 		if len(tabuleiro) == 0: break;
 		for y in range(x+1,9):
-			valido = verificarDuplicidadeVertical(tabuleiro[x], tabuleiro[y]) and verificarDuplicidadeHorizontal(tabuleiro[y]) and verificarDuplicidadeHorizontal(tabuleiro[x])
+			valido = (verificarDuplicidadeVertical(tabuleiro[x], tabuleiro[y]) and verificarDuplicidadeHorizontal(tabuleiro[y]) and verificarDuplicidadeHorizontal(tabuleiro[x]))
+			print ('valido' + str(valido))
 			if not valido:
-				del tabuleiro[0:]
-				break;
+				return []
 				#break;#gerarTabuleiro(count+1)
+	return tabuleiro
+
 cont = 0;
 while cont < 10000:
 	cont=cont+1
-	gerarTabuleiro()
+	tabuleiro = gerarTabuleiro()
+	print tabuleiro
 	if len(tabuleiro) > 0:
 		print 'tabuleiro gerado'
 		for x in tabuleiro:
 			print x
+		break;
