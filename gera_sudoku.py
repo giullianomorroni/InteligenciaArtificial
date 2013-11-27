e9a973cff603db67ffd2ca38feb280c4646e3d9c
giulliano <giulliano@debian.giulliano>
2013-11-26 23:59:52 -0200

    nova versao multi thread

--- gera_sudoku.py
+++ gera_sudoku.py
@@ -1,12 +1,40 @@
 #! /usr/bin/env python
 #-*- coding: utf-8 -*-
 
+from threading import Thread
 import sudoku as sd
 
-tabuleiro = sd.generate()
-while len(tabuleiro) == 0:
+def generateSudoku(threadName):
+	print 'starting thread ' + threadName
 	tabuleiro = sd.generate()
-	if len(tabuleiro) > 0:
-		print 'tabuleiro gerado'
-		for x in tabuleiro:
-			print x
+	count = 0
+	while len(tabuleiro) == 0 and count < 90000:
+		count = count+1
+		tabuleiro = sd.generate()
+		if len(tabuleiro) > 0:
+			print 'tabuleiro gerado'
+			print str(tabuleiro[0][0]) + ' ' + str(tabuleiro[1][0]) + ' ' + str(tabuleiro[2][0]) 
+			print str(tabuleiro[0][1]) + ' ' + str(tabuleiro[1][1]) + ' ' + str(tabuleiro[2][1]) 
+			print str(tabuleiro[0][2]) + ' ' + str(tabuleiro[1][2]) + ' ' + str(tabuleiro[2][2])
+
+			print str(tabuleiro[3][0]) + ' ' + str(tabuleiro[4][0]) + ' ' + str(tabuleiro[5][0]) 
+			print str(tabuleiro[3][1]) + ' ' + str(tabuleiro[4][1]) + ' ' + str(tabuleiro[5][1]) 
+			print str(tabuleiro[3][2]) + ' ' + str(tabuleiro[4][2]) + ' ' + str(tabuleiro[5][2])
+
+			print str(tabuleiro[6][0]) + ' ' + str(tabuleiro[7][0]) + ' ' + str(tabuleiro[8][0]) 
+			print str(tabuleiro[6][1]) + ' ' + str(tabuleiro[7][1]) + ' ' + str(tabuleiro[8][1]) 
+			print str(tabuleiro[6][2]) + ' ' + str(tabuleiro[7][2]) + ' ' + str(tabuleiro[8][2])
+	print threadName + ' finished'
+			
+thread1 = Thread(target = generateSudoku, args = ('thread1',))
+thread2 = Thread(target = generateSudoku, args = ('thread2',))
+thread3 = Thread(target = generateSudoku, args = ('thread3',))
+thread4 = Thread(target = generateSudoku, args = ('thread4',))
+thread5 = Thread(target = generateSudoku, args = ('thread5',))
+
+
+thread1.start()
+thread2.start()
+thread3.start()
+thread4.start()
+thread5.start()
--- sudoku.py
+++ sudoku.py
@@ -3,32 +3,141 @@
 
 import random
 
-sudoku = []
+def is_duplicated(contador):
+	duplicated = False
+	#print ' '
+	for d in range(1,9): 
+		#print str(d) + ' ' + str(contador.count(d))
+		if contador.count(d) > 1:
+			duplicated = True
+			#print ' duplicados ' + str(contador)
+			return duplicated
+	return duplicated
 
-for i in range(9):
-	numbers = [1,2,3,4,5,6,7,8,9]
-	random.shuffle(numbers)
+def generate():
+	sudoku = []
 
-	row = []
-	row.append(numbers.pop())
-	row.append(numbers.pop())
-	row.append(numbers.pop())
-	row.append(numbers.pop())
-	row.append(numbers.pop())
-	row.append(numbers.pop())
-	row.append(numbers.pop())
-	row.append(numbers.pop())
-	row.append(numbers.pop())
+	for i in range(9):
+		numbers = [1,2,3,4,5,6,7,8,9]
+		random.shuffle(numbers)
 
-	square = [[],[],[]]
+		row = []
+		row.append(numbers.pop())
+		row.append(numbers.pop())
+		row.append(numbers.pop())
+		row.append(numbers.pop())
+		row.append(numbers.pop())
+		row.append(numbers.pop())
+		row.append(numbers.pop())
+		row.append(numbers.pop())
+		row.append(numbers.pop())
 
-	square[0] = row[0:3]
-	square[1] = row[3:6]
-	square[2] = row[6:9]
+		square = [[],[],[]]
 
-	sudoku.append(square)
+		square[0] = row[0:3]
+		square[1] = row[3:6]
+		square[2] = row[6:9]
 
-for s in sudoku:
-	print ''
-	for r in s:
-		print r
+		sudoku.append(square)
+
+	#easy mode
+	for i in range(1,100):
+		remove_on = random.randint(1,2)
+		if remove_on == 2:
+			#print sudoku[random.randint(0,8)][random.randint(0,2)][random.randint(0,2)]
+			sudoku[random.randint(0,8)][random.randint(0,2)][random.randint(0,2)] = 0
+
+	# Analise na vertical
+	for coluna in range(0,3):
+		contador = []
+		contador.append(sudoku[0][0][coluna])
+		contador.append(sudoku[0][1][coluna])
+		contador.append(sudoku[0][2][coluna])
+
+		contador.append(sudoku[3][0][coluna])
+		contador.append(sudoku[3][1][coluna])
+		contador.append(sudoku[3][2][coluna])
+
+		contador.append(sudoku[6][0][coluna])
+		contador.append(sudoku[6][1][coluna])
+		contador.append(sudoku[6][2][coluna])
+		if is_duplicated(contador): return []
+
+	for coluna in range(0,3):
+		contador = []
+		contador.append(sudoku[1][0][coluna])
+		contador.append(sudoku[1][1][coluna])
+		contador.append(sudoku[1][2][coluna])
+
+		contador.append(sudoku[4][0][coluna])
+		contador.append(sudoku[4][1][coluna])
+		contador.append(sudoku[4][2][coluna])
+
+		contador.append(sudoku[7][0][coluna])
+		contador.append(sudoku[7][1][coluna])
+		contador.append(sudoku[7][2][coluna])
+		if is_duplicated(contador): return []
+
+	for coluna in range(0,3):
+		contador = []
+		contador.append(sudoku[2][0][coluna])
+		contador.append(sudoku[2][1][coluna])
+		contador.append(sudoku[2][2][coluna])
+
+		contador.append(sudoku[5][0][coluna])
+		contador.append(sudoku[5][1][coluna])
+		contador.append(sudoku[5][2][coluna])
+
+		contador.append(sudoku[8][0][coluna])
+		contador.append(sudoku[8][1][coluna])
+		contador.append(sudoku[8][2][coluna])
+		if is_duplicated(contador): return []
+
+
+	# Analise na horizontal
+	for coluna in range(0,3):
+		contador = []
+		contador.append(sudoku[0][coluna][0])
+		contador.append(sudoku[0][coluna][1])
+		contador.append(sudoku[0][coluna][2])
+
+		contador.append(sudoku[1][coluna][0])
+		contador.append(sudoku[1][coluna][1])
+		contador.append(sudoku[1][coluna][2])
+
+		contador.append(sudoku[2][coluna][0])
+		contador.append(sudoku[2][coluna][1])
+		contador.append(sudoku[2][coluna][2])
+		if is_duplicated(contador): return []
+
+	for coluna in range(0,3):
+		contador = []
+		contador.append(sudoku[3][coluna][0])
+		contador.append(sudoku[3][coluna][1])
+		contador.append(sudoku[3][coluna][2])
+
+		contador.append(sudoku[4][coluna][0])
+		contador.append(sudoku[4][coluna][1])
+		contador.append(sudoku[4][coluna][2])
+
+		contador.append(sudoku[5][coluna][0])
+		contador.append(sudoku[5][coluna][1])
+		contador.append(sudoku[5][coluna][2])
+		if is_duplicated(contador): return []
+
+	for coluna in range(0,3):
+		contador = []
+		contador.append(sudoku[6][coluna][0])
+		contador.append(sudoku[6][coluna][1])
+		contador.append(sudoku[6][coluna][2])
+
+		contador.append(sudoku[7][coluna][0])
+		contador.append(sudoku[7][coluna][1])
+		contador.append(sudoku[7][coluna][2])
+
+		contador.append(sudoku[8][coluna][0])
+		contador.append(sudoku[8][coluna][1])
+		contador.append(sudoku[8][coluna][2])
+		if is_duplicated(contador): return []
+		
+	return sudoku
